559c2c69dd82ddea10a4e5c78e7de1908d2f78aa
lucasx7 <lucasolix7@gmail.com>
2026-09-23 15:28:47 -0300

    adicionando projeto backend com ktor

---
+++ backend/src/main/kotlin/Serialization.kt
@@ -0,0 +1,9 @@
+package com.lucasx7.linksaver
+
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+    }
+}
---
+++ backend/src/main/kotlin/main.kt
@@ -0,0 +1,5 @@
+package com.lucasx7.linksaver
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
